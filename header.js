d3c420d6d3989b80c763c147e6d0b20d36162f6c
Waldemar Schröder <waldemar_schroeder@outlook.de>
2024-08-10 21:41:29 +0200

    new header

--- header.js
+++ header.js
@@ -1,25 +1,23 @@
-function addFields() {
-    const form = document.getElementById("rateForm");
+const form = document.getElementById("rateForm");
 
-    const fields = [
-        { label: 'EUR/DOL', name: 'eur_dol' },
-        { label: 'Gold', name: 'gold' },
-        { label: 'DOL/YEN', name: 'dol_yen' }
-    ];
+const fields = [
+    { label: 'EUR/DOL', name: 'eur_dol' },
+    { label: 'Gold', name: 'gold' },
+    { label: 'DOL/YEN', name: 'dol_yen' }
+];
 
-    fields.forEach(field => {
-        const div = document.createElement("div");
-        div.classList.add("form-group");
+fields.forEach(field => {
+    const div = document.createElement("div");
+    div.classList.add("form-group");
 
-        const label = document.createElement("label");
-        label.innerText = field.label;
-        div.appendChild(label);
+    const label = document.createElement("label");
+    label.innerText = field.label;
+    div.appendChild(label);
 
-        const input = document.createElement("input");
-        input.type = "text";
-        input.name = field.name;
-        div.appendChild(input);
+    const input = document.createElement("input");
+    input.type = "text";
+    input.name = field.name;
+    div.appendChild(input);
 
-        form.appendChild(div);
-    });
-}
+    form.appendChild(div);
+});
