aabbdbf99019c3e339bf2780e90fcaa781e78b00
Waldemar Schröder <waldemar_schroeder@outlook.de>
2024-08-10 22:31:37 +0200

    better design

--- header.js
+++ header.js
@@ -1,23 +1,30 @@
-const form = document.getElementById("rateForm");
+// Code wird beim Laden der Seite ausgeführt
+const form = document.getElementById("headerInfos");
 
 const fields = [
-    { label: 'EUR/DOL', name: 'eur_dol' },
-    { label: 'Gold', name: 'gold' },
-    { label: 'DOL/YEN', name: 'dol_yen' }
+    { label: 'S&P 500', value: '5,344.16' },
+    { label: 'DOW', value: '39,497.54' },
+    { label: 'EUR/USD', value: '1.09' },
+    { label: 'Gold', value: '2,430.80 USD' },
+    { label: 'USD/JPY', value: '146.67' }
 ];
 
 fields.forEach(field => {
     const div = document.createElement("div");
     div.classList.add("form-group");
 
+    const container = document.createElement("div");
+    container.classList.add("container");
+
     const label = document.createElement("label");
     label.innerText = field.label;
-    div.appendChild(label);
+    container.appendChild(label);
 
-    const input = document.createElement("input");
-    input.type = "text";
-    input.name = field.name;
-    div.appendChild(input);
+    const value = document.createElement("span");
+    value.classList.add("value");
+    value.innerText = field.value;
+    container.appendChild(value);
 
+    div.appendChild(container);
     form.appendChild(div);
 });
